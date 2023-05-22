73bc7ff8c44c68c719a8898d9599bb4822bb6495
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-22 19:53:25 -0300

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Todo.css'
+
+function Todo(){
+    return (
+        <div className="container">
+            <h1>Todo</h1>
+        </div>
+    )
+}
+
+export default Todo;
