96cd1cf3567d93ed31f30920a9501d32986cf00a
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-22 22:28:15 -0300

    add: function addItem, and send it to the list

--- src/Todo.js
+++ src/Todo.js
@@ -3,22 +3,37 @@ import './Todo.css'
 
 function Todo(){
 
-    const [text, setText] = useState("")
+    const [text, setText] = useState("");
+    const [items, setItems] = useState([]);
 
     function handleChange(event){
         let t = event.target.value;
-        setText(t)
+        setText(t);
+    }
+
+    function addItem(event){
+        event.preventDefault();
+
+        if(!text){
+            window.alert("Type something to add on the list.")
+        } else {
+            setItems([...items, text]);
+            setText("");
+        }
+
     }
 
     return (
         <div className="container">
             <h1>Todo</h1>
             <form>
-                <input onChange={handleChange} type="text"></input>
-                <button>Add</button>
-            </form>
+                <input onChange={handleChange} type="text" value={text}></input>
+                { <button onClick={addItem}>Add</button>}
+                {/* Este onClick no form tenta enviar o form/action.
+                Vamos evitar esse comportamento com preventDefault() dentro da função addItem */}
+            </form> 
             <ul>
-                <li>{text}</li>
+                 {items.map((item, index) =><li key={index}>{item}</li>)} {/*Cada item retorna uma linha */}
             </ul>
         </div>
     )
