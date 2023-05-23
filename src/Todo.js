0dc655417d6647319d4453929f0b686848d53e76
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-22 21:54:12 -0300

    onChange input text

--- src/Todo.js
+++ src/Todo.js
@@ -2,9 +2,22 @@ import React from "react";
 import './Todo.css'
 
 function Todo(){
+
+    function handleChange(event){
+        let text = event.target.value;
+        console.log(text)
+    }
+
     return (
         <div className="container">
             <h1>Todo</h1>
+            <form>
+                <input onChange={handleChange} type="text"></input>
+                <button>Add</button>
+            </form>
+            <ul>
+                <li>Item 1</li>
+            </ul>
         </div>
     )
 }
