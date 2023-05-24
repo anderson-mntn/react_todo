b11925c393362e06dece0e6e587f6dad9e16f707
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-23 21:49:35 -0300

    Seperando em arquivos (componente do commit anterior)

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './Todo.css'
 import List from './List'
+import TodoForm from "./TodoForm";
 
 function Todo(){
 
@@ -19,44 +20,7 @@ function Todo(){
     )
 }
 
-function TodoForm(props){
-    const [text, setText] = useState("");
 
-    function handleChange(event){
-        let t = event.target.value;
-        setText(t);
-    }
-
-    function addItem(event){
-        event.preventDefault();
-
-        if(!text){
-            window.alert("Type something to add on the list.")
-        } else {
-            // setItems([...items, text]);
-            props.onAddItem(text);
-            setText("");
-        }
-
-        /*
-            Um modo de printar um array dos itens ou conseguir manusear, sem o [] printa apenas um.
-
-            [items].forEach(element => {
-                console.log(element)
-            });
-        */
-    
-    }
-
-    return(
-        <form>
-            <input onChange={handleChange} type="text" value={text}></input>
-            { <button onClick={addItem}>Add</button>}
-            {/* Este onClick no form tenta enviar o form/action.
-            Vamos evitar esse comportamento com preventDefault() dentro da função addItem */}
-        </form> 
-    )
-}
 
 
 export default Todo;
---
+++ src/TodoForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function TodoForm(props){
+    const [text, setText] = useState("");
+
+    function handleChange(event){
+        let t = event.target.value;
+        setText(t);
+    }
+
+    function addItem(event){
+        event.preventDefault();
+
+        if(!text){
+            window.alert("Type something to add on the list.")
+        } else {
+            // setItems([...items, text]);
+            props.onAddItem(text);
+            setText("");
+        }
+
+        /*
+            Um modo de printar um array dos itens ou conseguir manusear, sem o [] printa apenas um.
+
+            [items].forEach(element => {
+                console.log(element)
+            });
+        */
+    
+    }
+
+    return(
+        <form>
+            <input onChange={handleChange} type="text" value={text}></input>
+            { <button onClick={addItem}>Add</button>}
+            {/* Este onClick no form tenta enviar o form/action.
+            Vamos evitar esse comportamento com preventDefault() dentro da função addItem */}
+        </form> 
+    )
+}
+
+export default TodoForm;
