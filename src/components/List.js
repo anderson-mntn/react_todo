c760cc1255acf83c93a4c459b05e840effd2162b
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-28 14:11:42 -0300

    add: buttons background opacity

--- src/components/List.js
+++ src/components/List.js
@@ -6,10 +6,10 @@ function List(props){
     function DoneIcon(props){
         if(props.done){
             console.log('done')
-            return (<img alt="done" src="./assets/done.png"></img>)
+            return (<img alt="done" src="./assets/done3.png"></img>)
         } else {
             console.log('undone')
-            return (<img alt="undone" src="./assets/undone.png"></img>)
+            return (<img alt="undone" src="./assets/undone2.png"></img>)
         }
     }
 
@@ -21,7 +21,7 @@ function List(props){
                 <p className={item.done? "done item" : "item"}>{item.text}</p>
                 <div className="ul-btn">
                 <button onClick={()=>{props.onDone(item)}}><DoneIcon done={item.done}></DoneIcon></button>
-                <button onClick={()=>{props.onItemDeleted(item)}}><img alt="bin" src="./assets/bin.png"></img></button>
+                <button onClick={()=>{props.onItemDeleted(item)}}><img alt="bin" src="./assets/bin2.png"></img></button>
                 </div>
                 </Card>
             </li>)} {/*Cada item retorna uma linha */}
