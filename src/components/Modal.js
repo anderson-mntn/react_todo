869ce0ea1555a1a7d43afbf7e519a87c2f8491aa
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-29 22:16:52 -0300

    add: function on click to hide modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,23 @@
 import React from "react";
 import Card from "./Card";
+import { useState } from "react";
 
 function Modal(props){
+
+    const [show, setShow] = useState(true)
+
+    function hideModal(e){
+        let target = e.target;
+        if(target.id === 'modal'){
+            setShow(false)
+        }
+    }
+
     return(
-        <div className="modal">
+        <div id="modal" 
+        onClick={hideModal} 
+        className={show ? "modal" : "modal hide"}
+        >
             <Card className="cardModal">
                 {props.children}
             </Card>
