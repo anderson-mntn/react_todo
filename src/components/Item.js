ab7b76cc0ff3d287532fc7d4a84521505e1bbdcc
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-05-30 22:24:24 -0300

    toFixed(4)

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@
 class Item{
     // static lastId = 0; // variavel de classe
     constructor(text){
-        this.id = Math.random().toFixed(2);
+        this.id = Math.random().toFixed(4);
         this.text = text;
         this.done = false;
     }
